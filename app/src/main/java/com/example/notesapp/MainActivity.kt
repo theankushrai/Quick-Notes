fb66a25be8b5b0f516b32441e91698874bc3d8c4
theankushrai <iamankushrai@gmail.com>
2021-05-11 12:50:43 +0530

    1. Home fragment created 2. navigation added

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.findNavController(R.id.myNavHostFragment)
+
     }
+
 }
