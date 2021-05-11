6094bdb655e602033df121c17879316f898a6e4f
theankushrai <iamankushrai@gmail.com>
2021-05-11 14:03:17 +0530

    1.Database Created

---
+++ app/src/main/java/com/example/notesapp/Database/NotesDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.notesapp.Database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Notes::class], version = 1, exportSchema = false)
+abstract class NotesDatabase : RoomDatabase() {
+
+    abstract fun noteDao():NoteDao
+
+    companion object {
+
+        var notesDatabase: NotesDatabase? = null
+
+        @Synchronized
+        fun getDatabase(context: Context): NotesDatabase {
+            if (notesDatabase == null) {
+                notesDatabase = Room.databaseBuilder(
+                    context
+                    , NotesDatabase::class.java
+                    , "notes.db"
+                ).build()
+            }
+            return notesDatabase!!
+        }
+    }
+
+}
